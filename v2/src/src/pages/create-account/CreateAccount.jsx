6acc15c09fc9854984db3aa1e18a4144a9ab5caf
Diego Marques <diegoslmarques@gmail.com>
2023-11-10 18:05:06 -0300

    useEffect para consertar flexbox - refatoração

--- v2/src/src/pages/create-account/CreateAccount.jsx
+++ v2/src/src/pages/create-account/CreateAccount.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import { React, useEffect } from "react";
 
-import styles from "./CreateAccount.module.css";
-import { useNavigate } from "react-router-dom";
+import "./CreateAccount.css";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import InitialLogo from "../../components/initial-logo/InitialLogo";
 
 export default function CreateAccount() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const root = document.getElementById("root");
+
+    useEffect(() => {
+        if (location.pathname === ("/create-account")) {
+            root.classList.remove(...root.classList);
+            root.classList.add("initial_root");
+        }
+    }, [location, root])
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -14,29 +23,29 @@ export default function CreateAccount() {
     }
 
     return (
-    <div id={styles.acc_cont}>
+    <div id="acc_cont">
         <InitialLogo />
-        <section id={styles.create_acc}>
+        <section id="create_acc">
             <h1>Crie sua conta</h1>
             <form onSubmit={handleSubmit}>
-                <fieldset id={styles.personal_data}>
+                <fieldset id="personal_data">
                     <label htmlFor="name">Nome completo</label>
-                    <input type="text" id={styles.name} name="name" placeholder="Digite seu nome completo..." required/><br/>
+                    <input type="text" id="name" name="name" placeholder="Digite seu nome completo..." required/><br/>
                     <label htmlFor="tia">TIA</label>
-                    <input type="text" id={styles.tia} name="tia" placeholder="Digite seu TIA..." maxLength="8" required/><br/>
+                    <input type="text" id="tia" name="tia" placeholder="Digite seu TIA..." maxLength="8" required/><br/>
                     <label htmlFor="email">E-mail</label>
-                    <input type="email" id={styles.email} name="email" placeholder="Digite seu e-mail..." required/><br/>
+                    <input type="email" id="email" name="email" placeholder="Digite seu e-mail..." required/><br/>
                     <label htmlFor="cellphone">Celular</label>
-                    <input type="number" id={styles.cellphone} name="cellphone" placeholder="Digite seu número com DDD..." required/><br/>
+                    <input type="number" id="cellphone" name="cellphone" placeholder="Digite seu número com DDD..." required/><br/>
                     <label htmlFor="birthdate">Data de nascimento</label>
-                    <input type="date" id={styles.birthdate} name="birthdate" required/><br/>
+                    <input type="date" id="birthdate" name="birthdate" required/><br/>
                     <label htmlFor="password">Senha</label>
-                    <input type="password" id={styles.password} name="password" placeholder="Digite sua senha..." required/><br/>
+                    <input type="password" id="password" name="password" placeholder="Digite sua senha..." required/><br/>
                     <label htmlFor="password">Confirmar senha</label>
-                    <input type="password" id={styles.password_confirm} name="password_confirm" placeholder="Digite novamente sua senha..." required/><br/>
+                    <input type="password" id="password_confirm" name="password_confirm" placeholder="Digite novamente sua senha..." required/><br/>
                     <legend>Dados pessoais</legend>
                 </fieldset>
-                <input id={styles.button} type="submit" value="Criar conta"/>
+                <input id="button" type="submit" value="Criar conta"/>
             </form>
         </section>
     </div>
--- v2/src/src/pages/index/Index.jsx
+++ v2/src/src/pages/index/Index.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { React, useEffect } from "react";
 
-import styles from "./Index.module.css";
-import { Link, useNavigate } from "react-router-dom";
+import "./Index.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import InitialLogo from "../../components/initial-logo/InitialLogo";
 
 export default function Index() {
     const navigate = useNavigate();
+    const location = useLocation();
+    const root = document.getElementById("root");
+
+    useEffect(() => {
+        if (location.pathname === ("/")) {
+            root.classList.remove(...root.classList);
+            root.classList.add("initial_root");
+        }
+    }, [location, root])
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -13,25 +22,25 @@ export default function Index() {
     }
 
     return (
-    <div id={styles.login_cont}>
+    <div id="login_cont">
         <InitialLogo />
-        <section id={styles.login}>
+        <section id="login">
             <h1>Login</h1>
             <form onSubmit={handleSubmit}>
-                <fieldset id={styles.login_field}>
-                    <label htmlFor="email">E-mail</label>
-                    <input type="email" id={styles.email} name="email" placeholder="Digite seu e-mail..." required/><br/>
+                <fieldset id="login_field">
+                    <label htmlFor="email_account">E-mail</label>
+                    <input type="email" id="email_account" name="email_account" placeholder="Digite seu e-mail..." required/><br/>
                     <label htmlFor="password">Senha</label>
-                    <input type="password" id={styles.password} name="password" placeholder="Digite sua senha..." required/><br/>
+                    <input type="password" id="password" name="password" placeholder="Digite sua senha..." required/><br/>
                     <legend>Login</legend>
                 </fieldset>
-                <input id={styles.button} type="submit" value="Fazer login"/>
+                <input id="login_button" type="submit" value="Fazer login"/>
             </form>
-            <div id={styles.login_options}>
-                <Link id={styles.forgot_password} to="/password-reset">Esqueci minha senha</Link>
-                <Link id={styles.create_acc} to="/create-account">Criar conta</Link>
+            <div id="login_options">
+                <Link id="forgot_password" to="/password-reset">Esqueci minha senha</Link>
+                <Link id="create_account" to="/create-account">Criar conta</Link>
             </div>
-            <div id={styles.no_login}>
+            <div id="no_login">
                 <Link to="/home">Iniciar sem fazer login</Link>
             </div>
         </section>
