8cb95699347175b4ebebd3ca8395a4c87b0e0564
Diego Marques <diegoslmarques@gmail.com>
2023-11-26 18:32:49 -0300

    Organização inicial + password-reset

---
+++ v2-next/app/offer/confirm/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { React } from "react";
+import { useRouter } from "next/navigation";
+
+import "./page.css";
+import Logo from "../components/logo/Logo";
+
+export default function Page() {
+    const router = useRouter();
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
---
+++ v2-next/app/password-reset/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { React } from "react";
+import { useRouter } from "next/navigation";
+
+import "./page.css";
+import Logo from "../components/logo/Logo";
+
+export default function PasswordReset() {
+    const router = useRouter();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        router.push("/");
+    }
+
+    return (
+        <div id="pass_cont">
+            <Logo initial={true}/>
+            <section id="password_container">
+                <h1>Alterar senha</h1>
+                <form onSubmit={handleSubmit}>
+                    <fieldset id="reset_field">
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" id="email" name="email" placeholder="Confirme seu e-mail..." required/><br/>
+                        <label htmlFor="password">Nova senha</label>
+                        <input type="password" id="password" name="password" placeholder="Digite sua nova senha..." required/><br/>
+                        <label htmlFor="password_confirm">Confirmar senha</label>
+                        <input type="password" id="password_confirm" name="password_confirm" placeholder="Confirme sua nova senha..." required/><br/>
+                        <legend>Nova senha</legend>
+                    </fieldset>
+                    <input id="button" type="submit" value="Confirmar"/>
+                </form>
+            </section>
+        </div>
+    );
+}
